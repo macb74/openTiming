c9289b6e7a883e443e889d06b9e6442ff11d7a54
Martin <martin@martin-bussmann.de>
2017-10-18 22:53:31 +0200

    alle zeiten der U10 um X Minuten reduzieren

--- help/helpingSQL.sql
+++ help/helpingSQL.sql
@@ -22,3 +22,9 @@ select count(*), event from marktlauf where veranstaltung = '2017' group by even
 select count(*), event, veranstaltung from marktlauf where veranstaltung = '2017' group by event
 union all
 select count(*), event, veranstaltung from marktlauf where veranstaltung = '2016' group by event order by event, veranstaltung;
+
+-- alle zeiten der U10
+SELECT * FROM `zeit` WHERE nummer in (SELECT stnr from teilnehmer where (klasse = 'MU10' or klasse = 'WU10') and vid = 19) and vid = 19 order by ID;
+
+-- alle zeiten der U10 um 1:20 Minuten reduzieren
+UPDATE `zeit` set `zeit` = zeit-120 WHERE nummer in (SELECT stnr from teilnehmer where (klasse = 'MU10' or klasse = 'WU10') and vid = 19) and vid = 19;
